30f5803e4240a1c86e44e6d5677eb4ea06b40c8d
briuin <dreamingdexiaoxiaohao@hotmail.com>
2025-01-16 01:42:59 +0800

    download image

--- src/app/components/success-modal/success-modal.component.ts
+++ src/app/components/success-modal/success-modal.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { UiButtonComponent } from '../../ui/ui-button/ui-button.component';
 import { Store } from '@ngrx/store';
 import { selectLastAddMultipleExhibitorResponse } from '../../exhibitor/store/exhibitor.selectors';
-import { filter, map, Observable } from 'rxjs';
+import { filter, map, Observable, tap } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-success-modal',
@@ -14,8 +15,9 @@ import { CommonModule } from '@angular/common';
   styleUrl: './success-modal.component.scss',
 })
 export class SuccessModalComponent {
+  @ViewChild('uniqueCode') uniqueCodeRef!: ElementRef;
   @Input() message: string = 'All exhibitors have been successfully added!';
-  uniqueId$!: Observable<string>;
+  uniqueId: string = '';
 
   constructor(
     public activeModal: NgbActiveModal,
@@ -23,11 +25,20 @@ export class SuccessModalComponent {
   ) {}
 
   ngOnInit() {
-    this.uniqueId$ = this.store
-      .select(selectLastAddMultipleExhibitorResponse)
-      .pipe(
-        filter((x) => x.length > 0),
-        map((x) => x[0].exhibitor.S_group_reg_id)
-      );
+    this.store.select(selectLastAddMultipleExhibitorResponse).pipe(
+      filter((x) => x.length > 0),
+      tap((x) => (this.uniqueId = x[0].exhibitor.S_group_reg_id))
+    ).subscribe();
+  }
+
+  downloadImage(): void {
+    const element = this.uniqueCodeRef.nativeElement;
+
+    html2canvas(element).then((canvas) => {
+      const link = document.createElement('a');
+      link.download = `registration-code-${this.uniqueId}.png`;
+      link.href = canvas.toDataURL('image/png');
+      link.click();
+    });
   }
 }
